e47d6271dc78b51f62e91519a3a2c93902c2f934
Cristhian Martinez <cristhianmartinez@SBSCO2s-MacBook-Pro.local>
2017-01-19 10:01:46 -0500

    First page and favicon

--- proyectos_express_gener/quiz/routes/index.js
+++ proyectos_express_gener/quiz/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz' }); //pasa el nombre de la vista y el objeto con las variables para renderizar
 });
 
 module.exports = router;
